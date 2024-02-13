c7d2fcaac1a0c025b08facd7e8bb917291decfd9
somni <me@somni.one>
2024-02-13 23:22:24 +0900

    Add web services list

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -2,7 +2,20 @@
   <main class="home">
     <h1>koharu.dev</h1>
     <h4>Site is under construction! 🐌</h4>
-    <RouterLink :to="{ name: 'about' }">About</RouterLink>
+
+    <div style="margin: 0.5em;">
+      <RouterLink :to="{ name: 'about' }">About</RouterLink>
+    </div>
+
+    <div style="margin: 2em 0.5em;">
+      <h5>Web services list</h5>
+
+      <div v-for="website in WEBSITES_LIST" :key="website.name" style="margin: 0.25em 0;">
+        <span>〉</span>
+        <a :href="website.url" style="margin: 0 0.25em;">{{ website.name }}</a>
+        <small style="font-size: smaller; font-weight: lighter; margin-left: 0.25em;"> — {{ website.description }}</small>
+      </div>
+    </div>
   </main>
 
   <img class="koharu-image" src="@/assets/koharu.png" alt="Shimoe Koharu official illustration" />
@@ -12,7 +25,26 @@
 import { Component, Vue } from "vue-facing-decorator";
 
 @Component({})
-export default class HomePage extends Vue { }
+export default class HomePage extends Vue {
+  readonly WEBSITES_LIST = [
+    {
+      name: "Mollufier",
+      description: "Turn sentences into 'Mollu'",
+      url: "https://mollufier.koharu.dev",
+    },
+    {
+      name: "MyBoothManager",
+      description: "Web-based booth publishment/management service for creators",
+      url: "https://booth.koharu.dev",
+    },
+
+    {
+      name: "Changelogs",
+      description: "Changelogs of koharu.dev web services",
+      url: "https://changelogs.koharu.dev",
+    },
+  ];
+}
 </script>
 
 <style lang="scss">
@@ -30,6 +62,7 @@ main.home {
   right: -10vh;
   object-fit: contain;
   object-position: bottom right;
+  opacity: 0.75;
 
   animation: koharu 7.5s infinite ease-in-out;
 }
